e0def2d3f82b7c4b567195cb8e0033acbf3e0f0f
Paolo Benvenuto <paolobenve@gmail.com>
2018-07-19 16:35:01 +0200

    cleaning

--- web/js/012-display.js
+++ web/js/012-display.js
@@ -566,18 +566,6 @@ $(document).ready(function() {
 			$("ul#right-menu li#album-search").addClass("hidden");
 			// $("ul#right-menu li#refine-search").addClass("hidden");
 		}
-
-		// if (! PhotoFloat.isSearchHash(location.hash))
-		// 	$("ul#right-menu li#refine-search").addClass("hidden");
-		var cleanedHash = PhotoFloat.cleanHash(location.hash);
-		// if (cleanedHash == '' || cleanedHash == Options.folders_string)
-		// 	$("ul#right-menu li#album-search").addClass("hidden");
-
-		// if (PhotoFloat.isSearchHash(location.hash) && $("ul#right-menu li#refine-search").hasClass("selected"))
-		// 	$("ul#right-menu li#album-search").addClass("dimmed");
-		// else
-		// 	$("ul#right-menu li#album-search").removeClass("dimmed");
-
 	}
 
 	function transformAltPlaceName(altPlaceName) {
