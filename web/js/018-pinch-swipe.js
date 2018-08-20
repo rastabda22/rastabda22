a9e7c13862724d94aff9fc91f108fd4dacbc4bd0
Paolo Benvenuto <paolobenve@gmail.com>
2018-08-20 13:29:36 +0200

    unnecessary instruction?

--- web/js/018-pinch-swipe.js
+++ web/js/018-pinch-swipe.js
@@ -533,7 +533,7 @@
         300,
         function() {
   				window.location.href = dest;
-  				$("#media-view").addClass("hidden");
+  				// $("#media-view").addClass("hidden");
   				$(mediaContainerSelector).css('top', "");
   			}
       );
