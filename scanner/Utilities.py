14d07e2acd2bb1489a0cfd18b3e0323395adc5ab
Paolo Benvenuto <paolobenve@gmail.com>
2019-01-01 19:05:15 +0100

    spacing

--- scanner/Utilities.py
+++ scanner/Utilities.py
@@ -228,7 +228,8 @@ def report_times(final):
 		print("                                  with geotags                       " + _num_photo_with_geotags.rjust(max_digit))
 		print()
 		print("                                  with exif date and geotags         " + _num_photo_with_exif_date_and_geotags.rjust(max_digit))
-		print()
+		if final:
+			print()
 		print("                                  with exif date but without geotags " + _num_photo_with_exif_date_and_without_geotags.rjust(max_digit))
 		if final:
 			for photo in Options.photos_with_exif_date_and_without_geotags:
