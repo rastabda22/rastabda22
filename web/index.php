de78224ca6208c033e7b6cff00642b416a39449b
Paolo Benvenuto <paolobenve@gmail.com>
2019-01-17 22:37:41 +0100

    add html code for map with markers

--- web/index.php
+++ web/index.php
@@ -34,6 +34,7 @@
 		<link href="css/001-fonts.css" rel="stylesheet" type="text/css" />
 		<link href="css/002-mobile.css" rel="stylesheet" type="text/css" />
 		<link href="css/003-social.css" rel="stylesheet" type="text/css" />
+		<link href="css/004-map-popup.css" rel="stylesheet" type="text/css" />
 	<?php	}
 
 			if (strcasecmp($options['debug_js'], "false") == 0 || $options['debug_js'] == "0") { ?>
@@ -75,12 +76,14 @@
 
 		<script type="text/javascript" src="js/006-jquery-touchswipe.js"></script>
 		<script type="text/javascript" src="js/007-jquery-lazyload.js"></script>
+		<script type="text/javascript" src="js/008-open-layers.js"></script>
 		<script type="text/javascript" src="js/010-social.js"></script>
 		<script type="text/javascript" src="js/012-translations.js"></script>
 		<script type="text/javascript" src="js/014-utilities.js"></script>
 		<script type="text/javascript" src="js/016-libphotofloat.js"></script>
 		<script type="text/javascript" src="js/018-pinch-swipe.js"></script>
 		<script type="text/javascript" src="js/020-display.js"></script>
+
 	<?php } ?>
 
 	<?php
@@ -205,6 +208,14 @@
 		<?php } ?>
 		</div>
 	<?php } ?>
+	</div>
+
+	<div class="map-container">
+		<span class="helper"></span>
+		<div>
+			<div class="map-close-button">X</div>
+			<div id="mapdiv"></div>
+		</div>
 	</div>
 
 	<div id="media-view">
