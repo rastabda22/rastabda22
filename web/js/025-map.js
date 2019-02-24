15ae53da0d2b3b9760c21f7a45cfa97de6386a86
Paolo Benvenuto <paolobenve@gmail.com>
2019-02-24 16:57:54 +0100

    add photo number to single point markers, from https://gist.github.com/comp615/2288108

--- web/js/025-map.js
+++ web/js/025-map.js
@@ -419,7 +419,13 @@
 				cacheBases += pointList[iPoint].mediaNameList[iPhoto].cacheBase;
 				}
 
-				markers[iPoint] = new PruneCluster.Marker(pointList[iPoint].lat, pointList[iPoint].long);
+				markers[iPoint] = new PruneCluster.Marker(
+					pointList[iPoint].lat,
+					pointList[iPoint].long,
+					{
+						icon:	new L.NumberedDivIcon({number: pointList[iPoint].mediaNameList.length})
+					}
+				);
 				pruneCluster.RegisterMarker(markers[iPoint]);
 				markers[iPoint].data.tooltip = cacheBases;
 				markers[iPoint].data.mediaNameList = pointList[iPoint].mediaNameList;
@@ -448,6 +454,40 @@
 			);
 		}
 	};
+
+	L.NumberedDivIcon = L.Icon.extend({
+		options: {
+			// EDIT THIS TO POINT TO THE FILE AT http://www.charliecroom.com/marker_hole.png (or your own marker)
+			iconUrl: 'css/images/marker_hole.png',
+			number: '',
+			shadowUrl: null,
+			iconSize: new L.Point(25, 41),
+			iconAnchor: new L.Point(13, 41),
+			popupAnchor: new L.Point(0, -33),
+			/*
+			iconAnchor: (Point)
+			popupAnchor: (Point)
+			*/
+			className: 'leaflet-div-icon'
+		},
+
+		createIcon: function () {
+			var div = document.createElement('div');
+			var img = this._createImg(this.options['iconUrl']);
+			var numdiv = document.createElement('div');
+			numdiv.setAttribute ( "class", "number" );
+			numdiv.innerHTML = this.options['number'] || '';
+			div.appendChild ( img );
+			div.appendChild ( numdiv );
+			this._setIconStyles(div, 'icon');
+			return div;
+		},
+
+		//you could change this to add a shadow like in the normal marker if you really wanted
+		createShadow: function () {
+			return null;
+		}
+	});
 
 	window.MapFunctions = MapFunctions;
 }());
