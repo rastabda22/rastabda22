73565fdbd568e84cc03eea468d403c088d12e2e4
Paolo Benvenuto <paolobenve@gmail.com>
2019-03-03 19:26:51 +0100

    function name

--- web/js/022-functions.js
+++ web/js/022-functions.js
@@ -533,7 +533,7 @@
 			return false;
 	};
 
-	Functions.expireInterval = function() {
+	Functions.threeYears = function() {
 		// returns the expire interval for the cookies, in seconds
 		// = 1000 days, ~ 3 years
 		return 1000 * 24 * 60 * 60;
@@ -541,7 +541,7 @@
 
 	Functions.setBooleanCookie = function(key, value) {
 		var expires = new Date();
-		expires.setTime(expires.getTime() + Functions.expireInterval() * 1000);
+		expires.setTime(expires.getTime() + Functions.threeYears() * 1000);
 		if (value)
 			value = 1;
 		else
@@ -568,7 +568,7 @@
 
 	Functions.prototype.setCookie = function(key, value) {
 		var expires = new Date();
-		expires.setTime(expires.getTime() + Functions.expireInterval() * 1000);
+		expires.setTime(expires.getTime() + Functions.threeYears() * 1000);
 		document.cookie = key + '=' + value + ';expires=' + expires.toUTCString();
 		return true;
 	};
