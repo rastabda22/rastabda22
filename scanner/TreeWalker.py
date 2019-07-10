31ce96126243c8851fbd7b905bdbe386b50a966f
Paolo Benvenuto <paolobenve@gmail.com>
2019-07-10 15:07:18 +0200

    cleaning

--- scanner/TreeWalker.py
+++ scanner/TreeWalker.py
@@ -130,11 +130,9 @@ class TreeWalker:
 			message("saving all unprotected albums to json files...", "", 4)
 			next_level()
 			save_password_codes()
+
 			for album in self.origin_album.subalbums:
-				# try:
 				self.all_albums_to_json_file(album)
-				# except UnboundLocalError:
-				# 	pass
 
 			back_level()
 			message("all unprotected albums saved to json files!", "", 5)
