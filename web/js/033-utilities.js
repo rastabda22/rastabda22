5d8c3636fb893cfe3cb7ba54a4113a706100b978
Paolo Benvenuto <paolobenve@gmail.com>
2020-11-11 20:53:07 +0100

    hide loading when showing the authorization dialog

--- web/js/033-utilities.js
+++ web/js/033-utilities.js
@@ -2330,6 +2330,7 @@
 
 	Utilities.prototype.showAuthForm = function(event, maybeProtectedContent = false) {
 		$("#album-view, #media-view, #my-modal, #no-results").css("opacity", "0.2");
+		$("#loading").hide();
 		$("#auth-text").stop().fadeIn(1000);
 		$("#password").focus();
 
